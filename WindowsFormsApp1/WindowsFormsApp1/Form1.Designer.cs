931acd06fa650ba24545dc21ce5c2c6572cf8951
DevNiceTeam <dxvsi123@gmail.com>
2022-07-30 22:29:29 +0300

    1
    1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 120);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(54, 123);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 103);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Укажите путь к папке Steam";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(473, 125);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Сохранить путь";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(340, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(54, 177);
+            this.textBox2.MaxLength = 4;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(264, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(478, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Укажите значение на которое будет отдалена камера желательно 1550";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(526, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 313);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.Extensions.Primitives;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        bool isCreated, fileExists, isWrited = false;
+        string dop = @"\steamapps\common\dota 2 beta\game\dota\bin\win64";
+        string fileName = @"\client.dll";
+        static string settingsFileName = "settings.properties";
+        string datePath = @"\steamapps\common\dota 2 beta\game\dota";
+        string dateFileName = @"\steam.inf";
+        string fullPath;
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(settingsFileName))
+            {                
+                txt("Открываю settings");
+                StreamReader sr = new StreamReader(settingsFileName); // читаем файл с помощью делиметра после открытия
+                char[] ch = { '\n', '=' };
+                var text = sr.ReadToEnd();   
+                
+
+
+
+
+
+                if (text.Equals("")) // Если файл настроек пустой то ничего пока не делаем 
+                {
+                    txt("Файл пустой");                   
+                }
+                else // Если файл настроек не пустой то cчитываем файл выбираем путь и записываем его в TextBox
+                {
+                    var line = text.Split(ch);                   
+                    textBox1.Text = line[1].Trim();
+                    textBox2.Text = line[3].Trim();
+                    isWrited = true;
+                    sr.Close();
+
+
+
+
+                    StreamReader str = new StreamReader(textBox1.Text + datePath + dateFileName);
+                    var text1 = str.ReadToEnd();
+                    txt(text1);
+                    txt("Файл не пустой"); //TODO:                    
+                } 
+            }
+            else
+            {
+                FileStream fst = new FileStream(settingsFileName, FileMode.Create);
+                txt("Создаю settings");
+                isCreated = true;
+                fst.Close();
+            }
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            using (CommonOpenFileDialog ofd = new CommonOpenFileDialog())
+            {
+                ofd.InitialDirectory = "";
+                ofd.IsFolderPicker = true;
+
+                if (ofd.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    textBox1.Text = ofd.FileName;
+                    fullPath = ofd.FileName;
+                    textBox1.Enabled = true;
+                    if (File.Exists(textBox1.Text + dop + fileName))
+                    {
+                        fileExists = true;
+                        textBox2.Enabled = true;
+                        button2.Enabled = true;
+                        checkBox1.Enabled = true;
+                        txt("Файл 1client.dll есть");
+                    }
+                    else
+                    {
+                        txt("Файла client.dll нет");
+                    }
+                }
+            }
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                txt("Записываю");
+                StreamWriter sw = new StreamWriter(settingsFileName);
+
+                sw.WriteLineAsync("Path = " + textBox1.Text);
+                sw.WriteAsync("Distance = " + textBox2.Text);
+                sw.Close();
+                isWrited = true;
+            }
+            else if(isWrited == true)
+            {
+                txt("уже записан");
+            }
+            else
+            {
+                txt("Не Записываю");
+            }
+        }
+
+        static void txt(String s)
+        {
+            Console.WriteLine(s);           
+        }
+    }
+}
